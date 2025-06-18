5c093e1ba6e90b41d91116b67200be4e21ee5d9c
chengyukang <chengyk@guideir.com>
2025-06-18 18:27:49 +0800

    开发Dropdown组件&Message组件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <header>
-    <Tooltip placement="right" :trigger="trigger" ref="tooltipRef" :open-delay="1000" :close-delay="1000">
+    <Dropdown
+      placement="right"
+      :trigger="trigger"
+      :menu-options="options"
+      @visible-change="(e) => inlineConsole('inline change', e)"
+      @select="(e) => inlineConsole('select', e)"
+    >
       <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
       <template #content>
         <h1>hello Tooltip</h1>
       </template>
-    </Tooltip>
+    </Dropdown>
   </header>
   <Icon icon="user-secret" />
   <Icon icon="arrow-up" size="2xl" type="danger" color="#2f987e" />
@@ -45,12 +51,16 @@
   </main>
 </template>
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, h } from "vue";
+import Dropdown from "./components/Dropdown/Dropdown";
+import type { MenuOption } from "./components/Dropdown/types";
 import Button from "./components/Button/Button.vue";
 import Collapse from "./components/Collapse/Collapse.vue";
 import Item from "./components/Collapse/CollapseItem.vue";
 import Icon from "./components/Icon/Icon.vue";
-import Tooltip from "./components/Tooltip/Tooltip.vue";
+// import Tooltip from "./components/Tooltip/Tooltip.vue";
+import Message from "./components/Message/Message.vue";
+import { createMessage } from "./components/Message/method";
 // import type { Options } from "@popperjs/core";
 import type { ButtonInstance } from "./components/Button/types";
 import type { TooltipInstance } from "./components/Tooltip/types";
@@ -58,26 +68,41 @@ import type { TooltipInstance } from "./components/Tooltip/types";
 const buttonRef = ref<ButtonInstance | null>(null);
 const tooltipRef = ref<TooltipInstance | null>(null);
 
+const options: MenuOption[] = [
+  { key: 1, label: h("b", "this is bold") },
+  { key: 2, label: "item2", disabled: true },
+  { key: 3, label: "item3", divided: true },
+  { key: 4, label: "item4" },
+];
+
 const openedValue = ref(["a"]);
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const trigger = ref<any>("click");
 // const options:Partial<Options> ={placement:'right-end',strategy:'fixed'}
 
-const open=()=>{
-  tooltipRef.value?.show()
-}
+const inlineConsole = (...args: any) => {
+  console.log(...args);
+};
 
-const close=()=>{
-  tooltipRef.value?.hide()
-}
+const open = () => {
+  tooltipRef.value?.show();
+};
+
+const close = () => {
+  tooltipRef.value?.hide();
+};
 
 onMounted(() => {
+createMessage({ message: 'hello world', duration: 0, showClose: true })
+  createMessage({ message: 'hello world again', duration: 0, type: 'success', showClose: true })
+  createMessage({ message: 'hello world three', duration: 0, type: 'danger', showClose: true })
   if (buttonRef.value) {
     console.log(buttonRef.value.ref);
   }
   setTimeout(() => {
     // trigger.value = "hover";
+    // instance.destory()
   }, 2000);
 });
 </script>
