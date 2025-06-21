0cacfc8bec7227d3611104c4428833a9094a189d
ldyllic <ldyllic_cyk@foxmail.com>
2025-06-21 18:08:09 +0800

    使用vitepress生成组件文档

---
+++ docs/demo/Button/Basic.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <Button ref="buttonRef">Test Button</Button>
+  <Button circle>VK</Button>
+  <Button disabled>Disabled Button</Button><br /><br />
+  <Button type="primary">Primary</Button>
+  <Button type="success">Success</Button>
+  <Button type="warning">Warning</Button>
+  <Button type="danger">Danger</Button><br /><br />
+  <Button type="primary" plain>Primary</Button>
+  <Button type="success" plain>Success</Button>
+  <Button type="info" plain>Info</Button>
+  <Button type="warning" plain>Warning</Button>
+  <Button type="danger" plain>Danger</Button><br /><br />
+  <Button size="large" plain>Large</Button>
+  <Button size="small" plain>Small</Button><br /><br />
+  <Button size="large" loading>Loading</Button>
+  <Button size="large" icon="arrow-up">Arrow</Button><br /><br />
+</template>
+<script setup lang="ts">
+import Button from "../../../src/components/Button/Button.vue";
+</script>
